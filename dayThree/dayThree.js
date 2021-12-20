69894eb0496fd92008e7c1d56aa3826f4196aad7
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-20 20:39:19 +1300

    feat(dayThree): :white_check_mark: solve task two

--- dayThree/dayThree.js
+++ dayThree/dayThree.js
@@ -1,7 +1,6 @@
 const readInput = require('../readInput');
 
 const [OPEN, TREE] = ['.', '#'];
-const [RISE, RUN] = [1, 3];
 
 /** @type {string[]} */
 const map = readInput('./dayThree/input.txt', 'STRING')
@@ -14,13 +13,27 @@ const map = readInput('./dayThree/input.txt', 'STRING')
  */
 const resolveX = xIndex => (xIndex >= map[0].length) ? resolveX(xIndex - map[0].length) : xIndex;
 
-let [xIndex, yIndex, count] = [0, 0, 0];
+/**
+ * Count the number of trees a specified [rise, run] slope encounters in the map
+ * @param {[number, number]} param0 The [rise, run] of the slope to inspect
+ * @returns {number} The number of trees the specified slope encounters
+ */
+const countTrees = ([rise, run]) => {
+	let [xIndex, yIndex, count] = [0, 0, 0];
 
-while (yIndex < map.length) {
-	if (map[yIndex][resolveX(xIndex)] === TREE) count++;
+	while (yIndex < map.length) {
+		if (map[yIndex][resolveX(xIndex)] === TREE) count++;
 
-	yIndex += RISE;
-	xIndex += RUN;
-}
+		yIndex += rise;
+		xIndex += run;
+	}
 
-console.log(count);
+	return count;
+};
+
+const SLOPES = [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]];
+
+const answer = SLOPES.map(slope => countTrees(slope))
+	.reduce((product, value) => product * value);
+
+console.log(answer);
