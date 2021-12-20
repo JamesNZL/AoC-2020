491a91ac0157eed7552e6dbed807dbe237cebb15
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-20 19:14:32 +1300

    docs(readInput): :label: specify `any[]` as either `string[] | number[]`

--- readInput.js
+++ readInput.js
@@ -4,7 +4,7 @@ const fs = require('fs');
  * Read a specified file and return it as an array
  * @param {string} path The filename of the file to read
  * @param {'STRING' | 'NUMBER'} type The desired type of the returned array
- * @returns {any[]} The read file as an array
+ * @returns {string[] | number[]} The read file as an array
  */
 module.exports = (path, type) => {
 	const input = fs.readFileSync(path, { encoding: 'utf-8', flag: 'r' });
