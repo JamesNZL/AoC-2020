b226e77963b9f7125eaae23b0b349747ebe74750
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-20 19:37:34 +1300

    feat(dayTwo): :white_check_mark: solve task two

--- dayTwo/dayTwo.js
+++ dayTwo/dayTwo.js
@@ -6,7 +6,7 @@ const passwords = readInput('./dayTwo/input.txt', 'STRING');
 let count = 0;
 
 passwords.forEach(line => {
-	const [range, letter, password] = line.split(/:? /g);
+	/* const [range, letter, password] = line.split(/:? /g);
 
 	const [minimum, maximum] = range.split('-');
 
@@ -17,7 +17,14 @@ passwords.forEach(line => {
 		if (occurences >= minimum && occurences <= maximum) count++;
 	}
 
-	catch { null; }
+	catch { null; } */
+
+	const [indexes, letter, password] = line.split(/:? /g);
+
+	const matches = indexes.split('-')
+		.map(index => (password[index - 1] === letter) ? 1 : 0);
+
+	if (matches.reduce((sum, value) => sum + value) === 1) count++;
 });
 
 console.log(count);
