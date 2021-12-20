7088269513234a9c04ad661832b6e2710b363b5d
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-21 01:30:02 +1300

    feat(readInput): :sparkles: implement support for groupings within the file

--- readInput.js
+++ readInput.js
@@ -4,14 +4,17 @@ const fs = require('fs');
  * Read a specified file and return it as an array
  * @param {string} path The filename of the file to read
  * @param {'STRING' | 'NUMBER'} type The desired type of the returned array
+ * @param {boolean} [isGrouped=false] Whether the input file is grouped by newlines
  * @returns {string[] | number[]} The read file as an array
  */
-module.exports = (path, type) => {
+module.exports = (path, type, isGrouped = false) => {
 	const input = fs.readFileSync(path, { encoding: 'utf-8', flag: 'r' })
 		.trim();
 
 	return (type === 'STRING')
-		? input.split('\n')
+		? (isGrouped)
+			? input.split('\n\n')
+			: input.split('\n')
 		: input.split('\n')
 			.map(string => Number(string));
 };
