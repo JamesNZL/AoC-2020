19c97284f3b9ba306a76bd6adba346d93be66f80
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-21 15:46:06 +1300

    refactor(readInput)!: :recycle: hard-code `input.txt` as the filename
    BREAKING CHANGE: 'input.txt' must be removed from existing filepaths

--- readInput.js
+++ readInput.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 
 /**
  * Read a specified file and return it as an array
- * @param {string} path The filename of the file to read
+ * @param {string} folder The filename of the file to read
  * @param {'STRING' | 'NUMBER'} type The desired type of the returned array
  * @param {boolean} [isGrouped=false] Whether the input file is grouped by newlines
  * @returns {string[] | number[]} The read file as an array
  */
-module.exports = (path, type, isGrouped = false) => {
-	const input = fs.readFileSync(path, { encoding: 'utf-8', flag: 'r' })
+module.exports = (folder, type, isGrouped = false) => {
+	const input = fs.readFileSync(`./${folder}input.txt`, { encoding: 'utf-8', flag: 'r' })
 		.trim();
 
 	return (type === 'STRING')
