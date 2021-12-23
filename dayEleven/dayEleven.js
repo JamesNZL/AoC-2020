3c0843af0d333e82d8872c893dcdffc23b0a4016
James <44986932+JamesNZL@users.noreply.github.com>
2021-12-23 13:39:23 +1300

    feat(dayEleven): :white_check_mark: solve task two

--- dayEleven/dayEleven.js
+++ dayEleven/dayEleven.js
@@ -1,26 +1,72 @@
 const readInput = require('../readInput');
 
 const [FLOOR, EMPTY_SEAT, OCCUPIED_SEAT] = ['.', 'L', '#'];
-const [OCCUPY, VACATE] = [[0], [4, 5, 6, 7, 8]];
+const [OCCUPY, VACATE] = [[0], [5, 6, 7, 8]];
 
 /** @type {string[][]} */
 const seatLayout = readInput('dayEleven/', 'STRING')
 	.map(row => row.trim().split(''));
 
 /**
- * Extract the adjacent seats of a specified [row, column] seat from the provided seat array
+ * Traverse outwards from a specified [row, column] seat in the provided seat array in the specified direction
+ * @param {string[][]} seatArray The seat array to traverse
+ * @param {[number, number]} param1 The [rowIndex, columnIndex] of the specified seat
+ * @param {[number, number]} param2 The last delta distance that has been traversed
+ * @param {string} direction The direction in which to traverse
+ * - Valid values are combinations of the strings `top` | `bottom` | `left` | `right`
+ * - If `self`, the seat at the specified [row, column] is returned
+ * @returns {[undefined | null | string, [number, number]]} The [foundSeat, [xDelta, yDelta]] of this execution
+ * - If `foundSeat` is `undefined`, a floor tile was found and therefore must traverse further outwards
+ * - If `null`, the edge of the seatArray has been reached without finding a seat
+ */
+const traverseOutwards = (seatArray, [rowIndex, columnIndex], [xDelta, yDelta], direction) => {
+	// Perform the appropriate increments/decrements to xDelta and yDelta depending on the specified direction
+	if (direction.includes('top')) yDelta--;
+	if (direction.includes('bottom')) yDelta++;
+	if (direction.includes('left')) xDelta--;
+	if (direction.includes('right')) xDelta++;
+
+	let foundSeat = (direction === 'self') ? seatArray[rowIndex][columnIndex] : undefined;
+
+	try {
+		const seat = seatArray[rowIndex + yDelta][columnIndex + xDelta];
+
+		// If the element at the traversed index is undefined, i.e. out of range, return null
+		if (seat === undefined) throw undefined;
+		// If a valid seat was found, return it
+		else if (seat === EMPTY_SEAT || seat === OCCUPIED_SEAT) foundSeat = seat;
+	}
+
+	// If an error was thrown because the index(es) were out of range, return null
+	catch { foundSeat = null; }
+
+	// Return this execution's xDelta and yDelta so that it can be incremented/decremented upon if necessary
+	return [foundSeat, [xDelta, yDelta]];
+};
+
+/**
+ * Identify the first visible seats from a specified [row, column] seat in the provided seat array
  * @param {string[][]} seatArray The seat array to extract seats from
  * @param {[number, number]} param1 The [rowIndex, columnIndex] of the specified seat
- * @returns {string[][]} A seat array of the specified seat and its directly adjacent seats
+ * @returns {string[][]} A seat array of the specified seat and its first visible seats
  */
-const extractAdjacentSeats = (seatArray, [rowIndex, columnIndex]) => {
-	// Specify the range of rows and columns that are directly adjacent to the specified indices
-	const rowRange = [rowIndex - 1, rowIndex, rowIndex + 1];
-	const columnRange = [columnIndex - 1, columnIndex, columnIndex + 1];
+const identifyVisibleSeats = (seatArray, [rowIndex, columnIndex]) => {
+	const DIRECTIONS = ['topLeft', 'top', 'topRight', 'left', 'self', 'right', 'bottomLeft', 'bottom', 'bottomRight' ];
 
-	// Filter the 2D array for the rows and columns which are within the specified ranges
-	return seatArray.filter((_, _rowIndex) => rowRange.includes(_rowIndex))
-		.map(row => row.filter((__, _columnIndex) => columnRange.includes(_columnIndex)));
+	const foundSeats = DIRECTIONS.map(direction => {
+		let foundSeat = undefined;
+		let [xDelta, yDelta] = [0, 0];
+
+		// Iterate until the seat is found, or the edge is reached
+		while (foundSeat === undefined) {
+			[foundSeat, [xDelta, yDelta]] = traverseOutwards(seatArray, [rowIndex, columnIndex], [xDelta, yDelta], direction.toLowerCase());
+		}
+
+		return foundSeat;
+	});
+
+	// Re-structure the 1x9 array into a 2D 3x3 array to maintain compatibility
+	return [foundSeats.slice(0, 3), foundSeats.slice(3, 6), foundSeats.slice(6)];
 };
 
 /**
@@ -50,7 +96,7 @@ const mutateSeat = (seatArray, [rowIndex, columnIndex], seat) => {
 	if (seat === FLOOR) return FLOOR;
 
 	// Find the number of occupied surrounding seats
-	const occupiedSurroundingSeats = countOccupiedSeats(extractAdjacentSeats(seatArray, [rowIndex, columnIndex]), false, seat);
+	const occupiedSurroundingSeats = countOccupiedSeats(identifyVisibleSeats(seatArray, [rowIndex, columnIndex]), false, seat);
 
 	// Return the correct seat symbol based on the seat mutation rules
 	if (VACATE.includes(occupiedSurroundingSeats)) return EMPTY_SEAT;
